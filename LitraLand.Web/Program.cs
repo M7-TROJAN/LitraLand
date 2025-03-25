8b5cd538e99afd37845e91dcc890eebd03f29a2d
Mahmoud Mohamed <mahmoud.abdalaziz@outlook.com>
2025-03-26 00:10:28 +0200

    remove test

--- LitraLand.Web/Program.cs
+++ LitraLand.Web/Program.cs
@@ -12,7 +12,6 @@ namespace LitraLand.Web
     {
         public static async Task Main(string[] args)
         {
-            Console.Title = "LitraLand";
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.

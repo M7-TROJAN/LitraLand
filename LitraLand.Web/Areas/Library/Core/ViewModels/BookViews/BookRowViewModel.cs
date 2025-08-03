fa26d0c778ee879180a9f60931406443ca80caf5
Mahmoud Mohamed <mahmoud.abdalaziz@outlook.com>
2025-08-03 03:41:24 +0300

    Initial commit

---
+++ LitraLand.Web/Areas/Library/Core/ViewModels/BookViews/BookRowViewModel.cs
@@ -0,0 +1,23 @@
+﻿namespace LitraLand.Web.Areas.Library.Core.ViewModels.BookViews
+{
+    public class BookRowViewModel
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; } = null!;
+
+        public string Author { get; set; } = null!;
+
+        public string Publisher { get; set; } = null!;
+
+        public DateTime PublishedDate { get; set; }
+
+        public string? ImageThumbnailUrl { get; set; }
+
+        public string Hall { get; set; } = null!;
+
+        public bool IsAvailableForRental { get; set; }
+
+        public bool IsDeleted { get; set; }
+    }
+}
---
+++ LitraLand.Web/Extensions/FormCollectionExtensions.cs
@@ -0,0 +1,23 @@
+﻿using LitraLand.Domain.Dtos;
+
+namespace LitraLand.Web.Extensions
+{
+    public static class FormCollectionExtensions
+    {
+        public static GetFilteredDto GetFilters(this IFormCollection form)
+        {
+            var sortColumnIndex = form["order[0][column]"];
+
+            GetFilteredDto filteredDto = new GetFilteredDto(
+                    Skip: int.TryParse(form["start"], out int s) ? s : 0,
+                    PageSize: int.TryParse(form["length"], out int p) ? p : 10,
+                    SearchValue: form["search[value]"].ToString(),
+                    SortColumnIndex: sortColumnIndex!,
+                    SortColumn: form[$"columns[{sortColumnIndex}][name]"]!,
+                    SortColumnDirection: form["order[0][dir]"]!
+            );
+
+            return filteredDto;
+        }
+    }
+}
